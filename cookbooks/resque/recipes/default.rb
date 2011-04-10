d332e8fbff039272d72f29ec39a5495d7a82790a
Jason Shifflet <jason@pikimal.com>
2011-04-10 15:48:52 -0400

    Update to memcached

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -68,7 +68,7 @@ appname = 'rails_3_production'
 
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   run_for_app(appname) do |app_name, data|
-    template "/data/#{app}/shared/config/resque.yml" do
+    template "/data/#{app_name}/shared/config/resque.yml" do
       owner node[:owner_name]
       group node[:owner_name]
       mode 0644
