6cc798e09fec58c953df23ec54007944f46a0310
Jason Shifflet <jason@pikimal.com>
2011-04-10 14:57:56 -0400

    Trying memcache for util server again.

--- cookbooks/memcached/recipes/default.rb
+++ cookbooks/memcached/recipes/default.rb
@@ -9,7 +9,7 @@ node[:members].each do |app_name,data|
   user = node[:users].first
 
 case node[:instance_role]
- when "solo", "app", "app_master", "util"
+ when "solo", "app", "app_master"
  run_for_app(appname) do |app_name, data|
    template "/data/#{app_name}/shared/config/memcached_custom.yml" do
      source "memcached.yml.erb"
@@ -32,4 +32,19 @@ case node[:instance_role]
    end
  end
  end
+end
+
+if node[:instance_role].include?('util')
+  run_for_app(appname) do |app_name, data|
+   template "/data/#{app_name}/shared/config/memcached_custom.yml" do
+     source "memcached.yml.erb"
+     owner user[:username]
+     group user[:username]
+     mode 0744
+     variables({
+         :app_name => app_name,
+         :server_names => node[:members]
+     })
+   end
+  end
 end
