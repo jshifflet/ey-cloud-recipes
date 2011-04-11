bb5b8de98aa0d3ce48f2fed83a6a866e7d347801
Jason Shifflet <jason@pikimal.com>
2011-04-11 10:11:59 -0400

    Adding in sitemap cron job.

---
+++ cookbooks/sitemaps/recipes/default.rb
@@ -0,0 +1,7 @@
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+  cron "sitemaps generator" do
+    weekday  '5'
+    user    node[:owner_name]
+    command "cd /data/rails_3_production/current && RAILS_ENV=production rake environment pikimal:generate_piki_sitemaps"
+  end
+end
