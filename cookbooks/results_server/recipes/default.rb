f62fa4e7cc089232d368875c8297897bc2e3a7cf
Jason Shifflet <jason@pikimal.com>
2011-04-26 14:43:13 -0400

    Run commands in execute block for starting the results server.

--- cookbooks/results_server/recipes/default.rb
+++ cookbooks/results_server/recipes/default.rb
@@ -1,13 +1,19 @@
 app_name = 'rails3_staging'
 
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
-  command %Q{
-    cd /data/#{app_name}/current && nohup rake environment pikimal:stop_result_server &
-  }
-  command %Q{
-    cd /data/#{app_name}/current && nohup rake environment pikimal:start_result_server &
+  
+  execute 'stop results server' do
+    command %Q{
+      cd /data/#{app_name}/current && nohup rake environment pikimal:stop_result_server &
+    }
+  end
 
-  }
+  exectue 'start results server' do
+    command %Q{
+      cd /data/#{app_name}/current && nohup rake environment pikimal:start_result_server &
+
+    }
+  end  
 
 end
 
