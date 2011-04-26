559a9bbdcf29552b709194b6d9efca374bf642a5
Jason Shifflet <jason@pikimal.com>
2011-04-26 15:16:19 -0400

    Run commands in execute block for starting the results no new line in %Q.

--- cookbooks/results_server/recipes/default.rb
+++ cookbooks/results_server/recipes/default.rb
@@ -4,14 +4,13 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   
   execute 'stop results server' do
     command %Q{
-      cd /data/#{app_name}/current && nohup rake environment pikimal:stop_result_server &
+      cd /data/#{app_name}/current && nohup rake environment pikimal:stop_result_server
     }
   end
 
   execute 'start results server' do
     command %Q{
-      cd /data/#{app_name}/current && nohup rake environment pikimal:start_result_server &
-
+      cd /data/#{app_name}/current && nohup rake environment pikimal:start_result_server & 
     }
   end  
 
