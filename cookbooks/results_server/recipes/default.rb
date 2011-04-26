ea21099f43a2f8acf8deb308863cd303c2673d71
Jason Shifflet <jason@pikimal.com>
2011-04-26 17:39:44 -0400

    Trying result server with monit.

--- cookbooks/results_server/recipes/default.rb
+++ cookbooks/results_server/recipes/default.rb
@@ -1,15 +1,15 @@
 app_name = 'rails3_staging'
 
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
-  
-  execute 'stop results server' do
-    command %Q{
-      cd /data/#{app_name}/current && rake environment pikimal:stop_result_server}
+  template "/etc/monit.d/result_server.#{app_name}.monitrc" do
+    source "result_server.monitrc.erb"
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :user => node[:owner_name],
+    })
   end
-
-  execute 'start results server' do
-    command "cd /data/#{app_name}/current && rake environment pikimal:start_result_server"
-  end  
-
 end
 
