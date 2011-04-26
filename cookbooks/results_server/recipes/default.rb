1c86514559d37b7024e2926fb4b43407954cafbe
Jason Shifflet <jason@pikimal.com>
2011-04-26 16:30:55 -0400

    Remove & from chef result server commands

--- cookbooks/results_server/recipes/default.rb
+++ cookbooks/results_server/recipes/default.rb
@@ -4,12 +4,12 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   
   execute 'stop results server' do
     command %Q{
-      cd /data/#{app_name}/current && nohup rake environment pikimal:stop_result_server
+      cd /data/#{app_name}/current && rake environment pikimal:stop_result_server
     }
   end
 
   execute 'start results server' do
-    command "cd /data/#{app_name}/current && nohup rake environment pikimal:start_result_server &"
+    command "cd /data/#{app_name}/current && rake environment pikimal:start_result_server"
   end  
 
 end
