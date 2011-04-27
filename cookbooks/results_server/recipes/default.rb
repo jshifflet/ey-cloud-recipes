b9137410070efd2f97a7809aba21c42d0301cb1c
Jason Shifflet <jason@pikimal.com>
2011-04-27 11:00:52 -0400

    Adding in recipe definitions for results_server

---
+++ cookbooks/results_server/recipes/default.rb
@@ -0,0 +1,15 @@
+app_name = 'rails_3_production'
+
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+  template "/etc/monit.d/result_server.#{app_name}.monitrc" do
+    source "result_server.monitrc.erb"
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :user => node[:owner_name],
+    })
+  end
+end
+
