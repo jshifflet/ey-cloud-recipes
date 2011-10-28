26efaa75a55e3e61b951508d7b3555eda9958a6c
Jon Daniel <binarycleric@gmail.com>
2011-10-28 13:52:01 -0400

    Moved the result server into an init.d style script.

--- cookbooks/results_server/recipes/default.rb
+++ cookbooks/results_server/recipes/default.rb
@@ -1,6 +1,17 @@
 app_name = 'rails_3_production'
 
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+  template "/pikimal/bin/result_server.#{app_name}" do
+    source "result_server_bin.erb"
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0700
+    variables({
+      :app_name => app_name,
+      :user => node[:owner_name],
+    })
+  end
+  
   template "/etc/monit.d/result_server.#{app_name}.monitrc" do
     source "result_server.monitrc.erb"
     owner node[:owner_name]
