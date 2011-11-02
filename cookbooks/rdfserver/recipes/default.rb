d607e9be67e96e89ac4d6a7e430fde34f70e638e
Jon Daniel <binarycleric@gmail.com>
2011-11-02 17:06:50 -0400

    Utility servers get the rdf config as well.

--- cookbooks/rdfserver/recipes/default.rb
+++ cookbooks/rdfserver/recipes/default.rb
@@ -1,6 +1,6 @@
 appname = 'rails_3_production'
 
-if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
   run_for_app(appname) do |app_name, data|
     template "/data/#{app_name}/shared/config/rdfserver.yml" do
       owner node[:owner_name]
